96579d1ee87c602591365a105ca009bdc42bb0e5
taylan pekbilek <121013844+TaylanPekbilek@users.noreply.github.com>
2023-05-15 12:11:07 +0300

    Add files via upload

---
+++ script/tesısbılgılerı.js
@@ -0,0 +1,79 @@
+const headerEl = document.querySelector(".header");
+var iconlar= document.querySelector('.iconlar')
+var iconlar1= document.querySelector('.iconlar1')
+var iconlar2= document.querySelector('.iconlar2')
+var iconlar3= document.querySelector('.iconlar3')
+var yazi=document.querySelector('.yazi')
+var yazi1=document.querySelector('.yazi1')
+var yazi2=document.querySelector('.yazi2')
+var yazi3=document.querySelector('.yazi3')
+
+var mainimg=document.getElementById('imgg');
+var images=[  'images/hotel.jpg', 'images/loby.jpg', 'images/havuz.jpg' ,'images/hoteldeniz.jpg' ];
+var num=0;
+const auto=true;
+const IntervalTime=5000;
+let SlideInterval;
+
+
+
+
+
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      headerEl.classList.add("header-scrolled");
+      iconlar.style.color='white'
+      iconlar1.style.color='white' 
+      iconlar2.style.color='white'
+      iconlar3.style.color='white'
+      yazi.style.color='white' 
+      yazi1.style.color='white' 
+      yazi2.style.color='white' 
+      yazi3.style.color='white'
+    } else if (window.scrollY <= 50) {
+      headerEl.classList.remove("header-scrolled");
+      iconlar.style.color='white'
+      iconlar1.style.color='white' 
+      iconlar2.style.color='white'
+      iconlar3.style.color='white'
+      yazi.style.color='white' 
+      yazi1.style.color='white' 
+      yazi2.style.color='white' 
+      yazi3.style.color='white'
+    }
+  });
+
+
+  function next(){
+    num++
+    if(num>=images.length){
+        num=0,
+        mainimg.src=images[num]
+    }else{
+        mainimg.src=images[num]
+    }
+  }
+  
+  function back(){
+    num--
+    if(num<0){
+        num=images.length-1;
+        mainimg.src=images[num]
+    }else{
+        mainimg.src=images[num]
+    }
+  }
+  
+  if(auto){
+    SlideInterval=setInterval(next,IntervalTime)
+  }
+
+
+  function açacak(){
+    document.getElementById('gökyüzü').style.width="100%"
+  }
+  
+  function kapatacak(){
+    document.getElementById('gökyüzü').style.width="0%"
+  }
